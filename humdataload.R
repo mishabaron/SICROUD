67305272b0a597f63b0e7301b5947fb000b01377
Michael Baron <baron@american.edu>
2020-08-13 13:12:57 -0400

    Reads and plots updated COVID-19 counts

--- humdataload.R
+++ humdataload.R
@@ -1,4 +1,4 @@
-# Data from https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases  
+# Data from https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases 
 
 ConfirmedGlobal = read.csv(url("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv&filename=time_series_covid19_confirmed_global.csv"));
 DiedGlobal = read.csv(url("https://data.humdata.org/hxlproxy/api/data-preview.csv?url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv&filename=time_series_covid19_deaths_global.csv"));
